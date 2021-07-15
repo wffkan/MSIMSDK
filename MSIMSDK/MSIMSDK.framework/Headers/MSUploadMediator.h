07ac479a9471c6fc7688d517cb52004e633d9933
benny wang <409322456@qq.com>
2021-07-15 16:43:34 +0800

    1.0.9

--- MSIMSDK/MSIMSDK.framework/Headers/MSUploadMediator.h
+++ MSIMSDK/MSIMSDK.framework/Headers/MSUploadMediator.h
@@ -11,6 +11,18 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/** 附件类型*/
+typedef NS_ENUM(NSInteger ,MSUploadFileType){
+    
+    MSUploadFileTypeImage = 0, //图片
+    
+    MSUploadFileTypeVideo = 1,//视频
+    
+    MSUploadFileTypeVoice = 2,//音频
+    
+    MSUploadFileTypeAvatar = 3,//头像
+};
+
 typedef void(^normalSucc)(NSString *url);
 typedef void(^normalProgress)(CGFloat progress);
 typedef void(^normalFail)(NSInteger code, NSString *desc);
@@ -20,7 +32,7 @@ typedef void(^normalFail)(NSInteger code, NSString *desc);
 @optional
 
 - (void)ms_uploadWithObject:(id)object
-                   fileType:(MSIMMessageType)type
+                   fileType:(MSUploadFileType)type
                    progress:(normalProgress)progress
                        succ:(normalSucc)succ
                        fail:(normalFail)fail;
